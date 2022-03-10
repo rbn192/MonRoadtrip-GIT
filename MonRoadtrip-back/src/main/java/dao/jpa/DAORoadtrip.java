caf20599904faa49b11d567628243a92e9fd2ba0
Emilie <emilie.v0613@gmail.com>
2022-03-10 15:37:13 +0100

    DAO Compte + Roadtrip

--- MonRoadtrip-back/src/main/java/dao/jpa/DAORoadtrip.java
+++ MonRoadtrip-back/src/main/java/dao/jpa/DAORoadtrip.java
@@ -2,32 +2,49 @@ package dao.jpa;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import dao.IDAORoadtrip;
+import model.Compte;
 import model.Roadtrip;
+import util.Context;
 
 public class DAORoadtrip implements IDAORoadtrip {
 
 	@Override
 	public Roadtrip findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em  = Context.getSingleton().getEmf().createEntityManager();
+		Roadtrip r = em.find(Roadtrip.class, id);
+		em.close();
+		return r;
 	}
 
 	@Override
 	public List<Roadtrip> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = Context.getSingleton().getEmf().createEntityManager();
+		List<Roadtrip> roadtrips = em.createQuery("SELECT r from Roadtrip r").getResultList();
+		em.close();
+		return roadtrips;
 	}
 
 	@Override
-	public Roadtrip save(Roadtrip o) {
-		// TODO Auto-generated method stub
-		return null;
+	public Roadtrip save(Roadtrip r) {
+		EntityManager em  = Context.getSingleton().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		r = em.merge(r);
+		em.getTransaction().commit();
+		em.close();
+		return r;
 	}
 
 	@Override
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
+		EntityManager em  = Context.getSingleton().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		Roadtrip r = em.find(Roadtrip.class, id);
+		em.remove(r);
+		em.getTransaction().commit();
+		em.close();	
 		
 	}
 
