d2336d6729bdc65c6456fdc7e68b9c8fe5d6c9eb
LucieGuitton <lucie.guitton@outlook.fr>
2022-03-10 15:38:05 +0100

    DAO Logement/Etape + pom.xml

--- MonRoadtrip-back/src/main/java/dao/IDAOLogement.java
+++ MonRoadtrip-back/src/main/java/dao/IDAOLogement.java
@@ -1,8 +1,10 @@
 package dao;
 
+import java.util.List;
+
 import model.Logement;
 
 public interface IDAOLogement extends IDAO<Logement,Integer>{
 
-	
+	public List<Logement> findAllDisponibles();	
 }
