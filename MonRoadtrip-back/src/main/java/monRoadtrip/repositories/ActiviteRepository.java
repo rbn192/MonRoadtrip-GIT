8dce27ed1b742ff6eca4213d914841b896cd8074
Emilie <emilie.v0613@gmail.com>
2022-03-29 14:59:02 +0200

    ajout de methodes dans les repo

--- MonRoadtrip-back/src/main/java/monRoadtrip/repositories/ActiviteRepository.java
+++ MonRoadtrip-back/src/main/java/monRoadtrip/repositories/ActiviteRepository.java
@@ -1,9 +1,23 @@
 package monRoadtrip.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import monRoadtrip.model.Activite;
+import monRoadtrip.model.Organisateur;
 
 public interface ActiviteRepository extends JpaRepository<Activite, Integer> {
+	
+	@Modifying
+	@Transactional
+	@Query("delete from Activite a where a.organisateur=:organisateur")
+	void deleteByOrganisateur(@Param("organisateur") Organisateur organisateur);
+	
+	@Query("select a from Activite a where a.organisateur=:organisateur")
+	void getAllByOrganisateur(@Param("organisateur") Organisateur organisateur);
 
 }
