aabab27b561d6825f7e6ae9860abf041af27d9d8
Chaymae <chaymae.sati@gmail.com>
2022-03-29 12:26:30 +0200

    participant + roadtrip

---
+++ MonRoadtrip-back/src/main/java/monRoadtrip/services/RoadtripService.java
@@ -0,0 +1,43 @@
+package monRoadtrip.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import monRoadtrip.exceptions.RoadtripException;
+import monRoadtrip.model.Roadtrip;
+import monRoadtrip.repositories.RoadtripRepository;
+
+@Service
+public class RoadtripService {
+	
+	@Autowired
+	private RoadtripRepository roadtripRepo;
+
+	public List<Roadtrip> getAll() {
+		return roadtripRepo.findAll();
+	}
+
+	public Roadtrip getById(Integer id) {
+		return roadtripRepo.findById(id).orElseThrow(RoadtripException::new);
+	}
+	
+	public Roadtrip save(Roadtrip roadtrip) {
+		if (roadtrip.getId() != null) {
+			Roadtrip roadtripEnBase = getById(roadtrip.getId());
+			roadtrip.setVersion(roadtripEnBase.getVersion());
+		}
+		return roadtripRepo.save(roadtrip);
+	}
+
+	public void delete(Roadtrip roadtrip) {
+		delete(roadtrip.getId());
+	}
+
+	public void delete(Integer id) {
+		roadtripRepo.deleteById(id);
+	}
+	
+
+}
