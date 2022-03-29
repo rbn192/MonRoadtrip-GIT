1f93594852f64373c1bcd64073b83b8c9157b345
Emilie <emilie.v0613@gmail.com>
2022-03-29 14:47:53 +0200

    add deleteByClient / hote dans logement et resa

--- MonRoadtrip-back/src/main/java/monRoadtrip/repositories/ReservationRepository.java
+++ MonRoadtrip-back/src/main/java/monRoadtrip/repositories/ReservationRepository.java
@@ -1,9 +1,25 @@
 package monRoadtrip.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import monRoadtrip.model.Client;
+import monRoadtrip.model.Participant;
 import monRoadtrip.model.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Integer>{
+	
+	@Modifying
+	@Transactional
+	@Query("delete from Reservation r where r.client=:client")
+	void deleteByClient(@Param("client") Client client);
 
+	@Modifying
+	@Transactional
+	@Query("delete from Reservation r where r.participant=:participant")
+	void deleteByParticipant(@Param("participant") Participant participant);
 }
